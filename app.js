ea8244c3b6385dd3ec28e27869ef5930629cbe4a
Kendall Morgan <kuyukip@gmail.com>
2022-06-11 11:22:00 -0500

    completed information lookup

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ function mainMenu(person, people) {
             // Stop application execution
             return;
         case "test":
-            
+
             break;
         default:
             // Prompt user again. Another instance of recursion
@@ -143,7 +143,12 @@ function displayPeople(people) {
 function displayPerson(person) {
     let personInfo = `First Name: ${person.firstName}\n`;
     personInfo += `Last Name: ${person.lastName}\n`;
-    //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
+    personInfo += `Gender: ${person.gender}\n`;
+    personInfo += `Date of Birth: ${person.dob}\n`;
+    personInfo += `Height: ${person.height}\n`;
+    personInfo += `Weight: ${person.weight}\n`;
+    personInfo += `Eye Color: ${person.eyeColor}\n`;
+    personInfo += `Occupation: ${person.occupation}\n`;
     alert(personInfo);
 }
 // End of displayPerson()
